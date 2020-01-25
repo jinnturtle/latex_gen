901f24ee73d237ce6c0697ffaa9a421f003532ba
jinnturtle <egined@gmail.com>
2020-01-25 14:02:16 +0000

    custom output path support and better support of cross-directory invocation of the program

--- latex_gen.sh
+++ latex_gen.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+#==============================================================================#
+#==============================   latex_gen.sh   ==============================#
+#==============================================================================#
+# This script acts as a wrapper around a LaTeX compiler, simplifying and       #
+# automating some LaTeX workflow elements.                                     #
+#------------------------------------------------------------------------------#
+# Currently only supporting pdftex with .pdf output, but it should not take    #
+# too much work to add some other outputs and compilers if needed.             #
+#==============================================================================#
+
 version_maj=2
-version_min=0
+version_min=1
+version_fix=2
 
 # TODO - add a flag for destination of output file
 
@@ -9,33 +20,70 @@ function run_help () {
     echo "USAGE:"
     echo -e "\t$0 [flags]"
     echo "FLAGS:"
+    echo -e "\t-d: remove auxilary compilation data after compile"
     echo -e "\t-h: display help"
     echo -e "\t-i: input file (mandatory)"
     echo -e "\t-m: move auxilary compilation data to separate folder after compile"
-    echo -e "\t-d: remove auxilary compilation data after compile"
+    echo -e "\t-o: output file path/name (not implemented yet)"
     echo -e "\t-v: print version of this program"
 }
 
 function print_ver () {
-    echo "version: ${version_maj}.${version_min}"
+    echo "version: ${version_maj}.${version_min}.${version_fix}"
 }
 
-args=("$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9")
+function rm_generated_files () {
+    cd $in_filedir
 
-# vars used to determine what will done with auxilliary files after compile
+    local filename_nosuf=$(basename "${in_filename}" .tex)
+    rm -fv "$filename_nosuf.aux"
+    rm -fv "$filename_nosuf.pyg"
+    rm -fv "$filename_nosuf.out"
+    rm -fv "$filename_nosuf.toc"
+    rm -fv "$filename_nosuf.log"
+    rm -rvf "_minted-$filename_nosuf"
+
+    cd --
+}
+
+# cannot use --output-dir=<my_dir>, because some packages don't support this
+# e.g. minted
+function mv_generated_files () {
+    cd $in_filedir
+
+    local filename_nosuf=$(basename "${in_filename}" .tex)
+    local aux_dirname="${filename_nosuf}_aux_build_files"
+    rm -rf "${aux_dirname}"
+    mkdir -p "${aux_dirname}"
+    mv -fv "$filename_nosuf.aux" \
+       "$filename_nosuf.pyg" \
+       "$filename_nosuf.out" \
+       "$filename_nosuf.toc" \
+       "$filename_nosuf.log" \
+       "$aux_dirname"
+    mv -fv "_minted-$filename_nosuf" \
+       "$aux_dirname/"
+
+    cd --
+}
+
+
+# flags used to determine what to do with auxilliary files after compile
 aux_handling=""
 aux_move="m"
 aux_del="d"
 
 in_filepath=""
+out_filepath_override="" # override default output location
 
 # process args
+args=($@)
 for i in ${!args[@]}; do
     if [ "${args[$i]}" == "-h" ]; then
         run_help
         exit 0
     elif [ "${args[$i]}" == "-i" ]; then
-        in_filepath=${args[$((i+1))]}
+        in_filepath=$(realpath ${args[$((i+1))]})
     elif [ "${args[$i]}" == "-m" ]; then
         aux_handling="$aux_move"
     elif [ "${args[$i]}" == "-d" ]; then
@@ -43,6 +91,8 @@ for i in ${!args[@]}; do
     elif [ "${args[$i]}" == "-v" ]; then
         print_ver
         exit 0
+    elif [ "${args[$i]}" == "-o" ]; then
+        out_filepath_override="$(realpath ${args[$((i+1))]})"
     fi
 done
 
@@ -68,47 +118,21 @@ fi
 # debug stuff
 # echo "h: $flag_help m: $flag_move d: $flag_del"
 # echo "aux_han: $aux_handling"
-# echo "i: $in_filepath i_f: $in_filename i_d: $in_filedir" 
+# echo "i: $in_filepath i_f: $in_filename i_d: $in_filedir"
 # echo "stopping due to testing"
 # exit
-
-function rm_generated_files () {
-    local filename_nosuf=$(basename "${in_filename}" .tex)
-    rm -fv "$filename_nosuf.aux"
-    rm -fv "$filename_nosuf.pyg"
-    rm -fv "$filename_nosuf.out"
-    rm -fv "$filename_nosuf.toc"
-    rm -fv "$filename_nosuf.log"
-    rm -rvf "_minted-$filename_nosuf"
-}
-
-function mv_generated_files () {
-    local filename_nosuf=$(basename "${in_filename}" .tex)
-    local aux_dirname="${filename_nosuf}_aux_build_files"
-    rm -rf "${aux_dirname}"
-    mkdir -p "${aux_dirname}"
-    mv -fv "$filename_nosuf.aux" \
-       "$filename_nosuf.pyg" \
-       "$filename_nosuf.out" \
-       "$filename_nosuf.toc" \
-       "$filename_nosuf.log" \
-       "$aux_dirname"
-    mv -fv "_minted-$filename_nosuf" \
-       "$aux_dirname/"
-}
-
-#some latex modules are capricious and want the latex compiler to be called
-#   from the same directory where the source file is
-cd $in_filedir
 
 #remove auxilary latex files before we start, leftover *.aux has caused
 #   compilation problems before
 rm_generated_files
 
-
 # compile the file specified in $in_filename
 # (running several passes because of how latex compiler works)
 while [ $runs_done -lt $runs_needed ]; do
+    # some latex modules are capricious and want the latex compiler to be called
+    # from the same directory where the source file is
+    cd $in_filedir
+
     echo
     echo "run #$(($runs_done +1))"
     echo
@@ -121,13 +145,26 @@ while [ $runs_done -lt $runs_needed ]; do
         exit 1
     fi
     runs_done=$((++runs_done))
+
+    cd --
 done
 
-# decide what to do with auxilliary files
+# decide what to do with auxilliary files if anything
 if [ "$aux_handling" == "$aux_move" ]; then
     mv_generated_files
 elif [ "$aux_handling" == "$aux_del" ]; then
     rm_generated_files
+fi
+
+# move output file if output filepath override is set
+if [ "$out_filepath_override" != "" ]; then
+    cd $in_filedir
+
+    filename_nosuf=$(basename "${in_filename}" .tex)
+    default_outp=$(realpath "$filename_nosuf.pdf")
+    mv -v $default_outp $out_filepath_override
+
+    cd --
 fi
 
 exit 0
